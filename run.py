1f13973d3415723ccb7b1f82d67f9dbed9441957
Panni <dacheranna@gmail.com>
2020-10-25 16:34:54 +0100

    works fine, no qlearning

--- run.py
+++ run.py
@@ -1,24 +1,21 @@
 import time
 
 import gym
-
+from pygame.examples.vgrade import timer
+from stable_baselines3 import DQN
+from stable_baselines3.dqn import MlpPolicy
 
 import tl1
 
-
-def random_agent(episodes=100):
-    env = gym.make("tl1-v0")
-    env.reset()
+env = gym.make("tl1-v0")
+obs = env.reset()
+env.render()
+while True:
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
     env.render()
-    for e in range(episodes):
-        action = env.action_space.sample()
-        state, reward, done, _ = env.step(action)
-        env.render()
-        print(reward)
-        #time.sleep(1)
-        if done:
-            break
-
-
-if __name__ == "__main__":
-    random_agent()
+    time.sleep(1)
+    print("reward: ", reward)
+    if done:
+        obs = env.reset()
+        break
--- tl1/envs/tl1_env.py
+++ tl1/envs/tl1_env.py
@@ -1,6 +1,9 @@
 import sys
 from contextlib import closing
 
+from gym import spaces, logger
+
+import gym
 import numpy as np
 from io import StringIO
 
@@ -12,85 +15,78 @@ DOWN = 1
 RIGHT = 2
 UP = 3
 
-MAPS = {
-    "3x4": [
-        "FHFG",
-        "FFFF",
-        "SFHF"
-    ],
-}
 
-class tl1_env(discrete.DiscreteEnv):
+class tl1_env(gym.Env):
+    startPosition = [2, 0]
 
-    metadata = {'render.modes': ['human', 'ansi']}
+    done = False
 
-    def __init__(self, desc=None, map_name="3x4"):
-        if desc is None:
-            desc = MAPS[map_name]
+    lastaction = None
+
+    mymap = (
+        ("O", "X", "O", "G"),
+        ("O", "O", "O", "O"),
+        ("S", "O", "X", "O")
+    )
+
+    def __init__(self, mm=mymap, startpos=None):
+        if startpos is None:
+            startpos = self.startPosition
+        self.position = startpos
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(np.array([0, 0]), np.array([2, 3]), dtype=np.int)
+        desc = mm
         self.desc = desc = np.asarray(desc, dtype='c')
-        self.nrow, self.ncol = nrow, ncol = desc.shape
-        self.reward_range = (0, 1)
 
-        nA = 4
-        nS = nrow * ncol
+    def makestep(self, a):
+        col = self.position[1]
+        row = self.position[0]
+        if a == LEFT:
+            col = max(col - 1, 0)
+        elif a == DOWN:
+            row = min(row + 1, 2)
+        elif a == RIGHT:
+            col = min(col + 1, 3)
+        elif a == UP:
+            row = max(row - 1, 0)
+        return (row, col)
 
-        isd = np.array(desc == b'S').astype('float64').ravel()
-        isd /= isd.sum()
+    def step(self, action):
+        reward = 0
+        newpos = self.makestep(action)
+        self.position = newpos
+        newletter = self.desc[self.position[0], self.position[1]]
+        self.lastaction = action
+        if newletter == b'X':
+            reward = -5
+        elif newletter == b'G':
+            reward = 10
+            self.done = True
+        else:
+            reward = 0
+        return newpos, reward, self.done, {}
 
-        P = {s: {a: [] for a in range(nA)} for s in range(nS)}
-
-        def to_s(row, col):
-            return row*ncol + col
-
-        def inc(row, col, a):
-            if a == LEFT:
-                col = max(col - 1, 0)
-            elif a == DOWN:
-                row = min(row + 1, nrow - 1)
-            elif a == RIGHT:
-                col = min(col + 1, ncol - 1)
-            elif a == UP:
-                row = max(row - 1, 0)
-            return (row, col)
-
-        def update_probability_matrix(row, col, action):
-            newrow, newcol = inc(row, col, action)
-            newstate = to_s(newrow, newcol)
-            newletter = desc[newrow, newcol]
-            done = bytes(newletter) in b''
-            reward = float(newletter == b'GH')
-            return newstate, reward, done
-
-        for row in range(nrow):
-            for col in range(ncol):
-                s = to_s(row, col)
-                for a in range(4):
-                    li = P[s][a]
-                    letter = desc[row, col]
-                    if letter in b'H':
-                        li.append((1.0, s, -5, True))
-                    if letter in b'GH':
-                        li.append((1.0, s, 10, True))
-                    else:
-                          li.append((
-                            1.0, *update_probability_matrix(row, col, a)
-                           ))
-
-        super(tl1_env, self).__init__(nS, nA, P, isd)
+    def reset(self, pos=None):
+        if pos is None:
+            pos = self.startPosition
+        self.done = False
+        self.position = pos
+        self.lastaction = None
+        return self.position
 
     def render(self, mode='human'):
         outfile = StringIO() if mode == 'ansi' else sys.stdout
 
-        row, col = self.s // self.ncol, self.s % self.ncol
+        row, col = self.position[0], self.position[1]
         desc = self.desc.tolist()
         desc = [[c.decode('utf-8') for c in line] for line in desc]
-        desc[row][col] = utils.colorize(desc[row][col], "blue", highlight=True)
+        desc[row][col] = utils.colorize(desc[row][col], "cyan", highlight=True)
         if self.lastaction is not None:
             outfile.write("  ({})\n".format(
                 ["Left", "Down", "Right", "Up"][self.lastaction]))
         else:
             outfile.write("\n")
-        outfile.write("\n".join(''.join(line) for line in desc)+"\n")
+        outfile.write("\n".join(''.join(line) for line in desc) + "\n")
 
         if mode != 'human':
             with closing(outfile):
